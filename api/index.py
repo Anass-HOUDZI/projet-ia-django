793fc85eb18a7a95cbca6b9e4087b9615da1519d
nass <nassim.def@gmail.com>
2026-08-09 16:02:29 +0200

    fix ci

--- api/index.py
+++ api/index.py
@@ -4,4 +4,4 @@ import sys
 # Ensure root directory is on Python path
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from django_intro.wsgi import app
+from django_intro.wsgi import app  # noqa: F401
